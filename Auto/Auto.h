82bb75052966656aef051230682e36fd837648a8
Danym2407 <danym2407@gmail.com>
2025-01-28 13:25:13 -0600

    Terminamos el codigo de Auto y ahora entendemos como crear objectos e instancias.

---
+++ Auto/Auto.h
@@ -0,0 +1,16 @@
+//
+//  Auto.h
+//  Auto
+//
+//  Created by Daniela Mendez on 28/01/25.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Auto : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Auto/Auto.m
@@ -0,0 +1,12 @@
+//
+//  Auto.m
+//  Auto
+//
+//  Created by Daniela Mendez on 28/01/25.
+//
+
+#import "Auto.h"
+
+@implementation Auto
+
+@end
